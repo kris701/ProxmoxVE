32d25031623d5bd1b3c74a3a0a04224f73ecdd56
Kristian Skov <kris701kj@gmail.com>
2025-01-18 11:08:12 +0100

    tmp

---
+++ install/urbackup-install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: Kristian Skov
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+#update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get update
+wget https://hndl.urbackup.org/Server/2.5.33/urbackup-server_2.5.33_amd64.deb
+dpkg -i urbackup-server_2.5.33_amd64.deb
+$STD apt install -f
+msg_ok "Installed Dependencies"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
